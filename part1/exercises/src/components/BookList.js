89ef6dff74bb32376d50d8e9675e269de6f67883
Terri <404516+tpenn@users.noreply.github.com>
2023-07-03 19:57:06 -0500

    Part 1 done

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,17 @@
+import style from './BookList.module.css';
+
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book in Celebration of Sapphic Love and Internation Faerie Day! :D";
+   let book1 = "https://iheartsapphfic.com/WP/wp-content/uploads/2022/09/The-Faerie-Queen.jpg";
+   let book2 = "https://iheartsapphfic.com/WP/wp-content/uploads/2022/11/Diamond-Rough.jpg";
+   let book3 = "https://iheartsapphfic.com/WP/wp-content/uploads/2023/05/The-Fae-Queens-Captive.jpg";
 
    return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+      <div className={style.books}>
+         <h3 className={style.booksHeader}>{pageTitle}</h3>
+         <img className={style.booksImage} height="340" width="240" src={book1} alt="The Faerie Queen by Lucy True" />
+         <img className={style.booksImage} height="340" width="240" src={book2} alt="Diamond Rough by Serenity Snow" />
+         <img className={style.booksImage} height="340" width="240" src={book3} alt="The Fae Queen's Captive by Sierra Simone" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,14 @@
+import style from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <div>
+         <h3 className={style.choresHeading}>To Do List</h3>
+         <ol>
+            <li className={style.choresText}>Feed Cats</li>
+            <li className={style.choresText}>Clean</li>
+            <li className={style.choresText}>Water Plants</li>
+         </ol>
+      </div>
+   );
 }
